19726814229077f0492dfb903a528bbd61c7e06a
Mesut Piskin <mesutpiskin@outlook.com>
2018-10-25 13:36:07 +0300

    GOTURN Tracker Algoritması

--- code/yuz-tanima/python/facerecognition_kutuphanesi/facerec_with_gui.py
+++ code/yuz-tanima/python/facerecognition_kutuphanesi/facerec_with_gui.py
@@ -72,12 +72,7 @@ def faceRecognitionFromPicture(cvframe):
             name = known_face_names[first_match_index]
 
         face_names.append(name)
-    print("- Face Distance:")
-    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-    for i, face_distance in enumerate(face_distances):
-        print("The test image has a distance of {:.2} from known image #{}".format(face_distance, i))
-        print("- With a normal cutoff of 0.6, would the test image match the known image? {}".format(face_distance < 0.6))
-        print("- With a very strict cutoff of 0.5, would the test image match the known image? {}".format(face_distance < 0.5))
+    
         
     print("- Face Locations:")
     # print face data
